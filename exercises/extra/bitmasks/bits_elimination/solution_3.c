e6abb7209ac0792a84300091ff18b930e27f1176
bes01 <55254691+bes01@users.noreply.github.com>
2020-12-14 22:18:24 +0400

    Update solution_3.c

--- exercises/extra/bitmasks/bits_elimination/solution_3.c
+++ exercises/extra/bitmasks/bits_elimination/solution_3.c
@@ -2,6 +2,6 @@
 
 int EliminateBits(int num, int left_boundary, int right_boundary){
     int left = left_boundary == 31 ? 0 : (num >> left_boundary + 1) << (left_boundary + 1);
-    int right = num & (((1 << right_boundary) - 1));
+    int right = num & ((1 << right_boundary) - 1);
     return   num & (left + right);
 }
